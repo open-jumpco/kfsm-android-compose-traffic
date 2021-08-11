90396f8cc818c78dce91f3cff24f82fc2d535c46
corneil_jumpco <corneil@jumpco.io>
2021-08-11 15:05:19 +0200

    updated docs

--- app/src/main/java/com/example/kfsm/compose/trafficlight/TrafficIntersectionModel.kt
+++ app/src/main/java/com/example/kfsm/compose/trafficlight/TrafficIntersectionModel.kt
@@ -3,8 +3,8 @@ package com.example.kfsm.compose.trafficlight
 class TrafficIntersectionModel : TrafficIntersectionImplementation(
     listOf(
         TrafficLightModel("1"),
-        TrafficLightModel("2")/*,
-        TrafficLightModel("3")*/
+        TrafficLightModel("2"),
+        TrafficLightModel("3")
     )
 ) {
     override fun get(name: String): TrafficLightModel {
