2ceaeb5401ecda69278028ba65ce796e5df03876
corneil_jumpco <corneil@jumpco.io>
2021-08-28 00:47:27 +0200

    Added channels to the services.

--- app/src/main/java/com/example/kfsm/compose/traffic/MainActivity.kt
+++ app/src/main/java/com/example/kfsm/compose/traffic/MainActivity.kt
@@ -9,14 +9,16 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kfsm.compose.traffic.fsm.TrafficIntersectionService
 import com.example.kfsm.compose.traffic.fsm.TrafficLightService
 import com.example.kfsm.compose.traffic.theme.KFSMComposeTrafficLightTheme
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 private val intersectionModel = TrafficIntersectionService(
     listOf(
@@ -26,9 +28,14 @@ private val intersectionModel = TrafficIntersectionService(
     )
 )
 private var intersectionViewModel = TrafficIntersectionViewModel(intersectionModel)
-private var portraitMode: MutableState<Boolean> = mutableStateOf(true)
+private var portraitMode: MutableStateFlow<Boolean> = MutableStateFlow(true)
 
 class MainActivity : AppCompatActivity() {
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        portraitMode.value = Configuration.ORIENTATION_PORTRAIT == newConfig.orientation
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         CoroutineScope(Dispatchers.Default).launch {
@@ -36,8 +43,7 @@ class MainActivity : AppCompatActivity() {
         }
         setContent {
             MainWindow()
-            portraitMode.value =
-                Configuration.ORIENTATION_PORTRAIT == LocalConfiguration.current.orientation
+
         }
     }
 }
@@ -46,9 +52,11 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MainWindow() {
     KFSMComposeTrafficLightTheme {
+        val portrait =
+            portraitMode.collectAsState(Configuration.ORIENTATION_PORTRAIT == LocalConfiguration.current.orientation)
         Scaffold {
             Surface(color = MaterialTheme.colors.background) {
-                Intersection(intersectionViewModel, portraitMode.value)
+                Intersection(intersectionViewModel, portrait.value)
             }
         }
     }
